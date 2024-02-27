e43bb9d084070747d00f24401c085b8266778904
violettomania <danielpolya@protonmail.com>
2024-02-27 18:37:19 +0100

    rewrite styled components to .tsx

--- src/assets/wrappers/StatItem.tsx
+++ src/assets/wrappers/StatItem.tsx
@@ -1,6 +1,11 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
 
-const Wrapper = styled.article`
+interface WrapperProps {
+  color?: string;
+  bcg?: string;
+}
+
+const Wrapper = styled.article<WrapperProps>`
   padding: 2rem;
   background: var(--white);
   border-radius: var(--borderRadius);
@@ -36,6 +41,6 @@ const Wrapper = styled.article`
       color: ${(props) => props.color};
     }
   }
-`
+`;
 
-export default Wrapper
+export default Wrapper;
--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -1,24 +1,27 @@
 import main from '../assets/images/main.svg';
+import Wrapper from '../assets/wrappers/LandingPage';
 import { Logo } from '../components';
 
 const Landing = () => {
   return (
-    <main>
-      <nav>
-        <Logo />
-      </nav>
-      <div className='container page'>
-        {/* info */}
-        <div className='info'>
-          <h1>
-            job <span>tracking</span> app
-          </h1>
-          <p>some text</p>
-          <button className='btn btn-hero'>Login/Register</button>
+    <Wrapper>
+      <main>
+        <nav>
+          <Logo />
+        </nav>
+        <div className='container page'>
+          {/* info */}
+          <div className='info'>
+            <h1>
+              job <span>tracking</span> app
+            </h1>
+            <p>some text</p>
+            <button className='btn btn-hero'>Login/Register</button>
+          </div>
+          <img src={main} alt='job hunt' className='img main-img' />
         </div>
-        <img src={main} alt='job hunt' className='img main-img' />
-      </div>
-    </main>
+      </main>
+    </Wrapper>
   );
 };
 
