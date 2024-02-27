b75391827e85beea1b675853cc063bedb1187357
violettomania <danielpolya@protonmail.com>
2024-02-27 18:37:59 +0100

    complete landing page

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -15,7 +15,11 @@ const Landing = () => {
             <h1>
               job <span>tracking</span> app
             </h1>
-            <p>some text</p>
+            <p>
+              Crucifix narwhal street art asymmetrical, humblebrag tote bag
+              pop-up fixie raclette taxidermy craft beer. Brunch bitters synth,
+              VHS crucifix heirloom meggings bicycle rights
+            </p>
             <button className='btn btn-hero'>Login/Register</button>
           </div>
           <img src={main} alt='job hunt' className='img main-img' />
