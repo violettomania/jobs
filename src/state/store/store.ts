b441cfa45b5b329f019fe36418ce4ea74c870889
violettomania <danielpolya@protonmail.com>
2024-03-02 16:00:40 +0100

    remove sidebar slice

--- src/state/store/store.ts
+++ src/state/store/store.ts
@@ -1,14 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 
 import jobsSlice from '../slices/jobsSlice';
-import sidebarSlice from '../slices/sidebarSlice';
 import userSlice from '../slices/userSlice';
 
 export const store = configureStore({
   reducer: {
     user: userSlice,
     jobs: jobsSlice,
-    sidebar: sidebarSlice,
   },
 });
 
