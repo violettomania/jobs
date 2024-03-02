0594b3d3119df6bfb5a7bb5c8c00405b5cbb5702
violettomania <danielpolya@protonmail.com>
2024-03-02 15:36:17 +0100

    WIP add Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,44 @@
-import { FaHome } from 'react-icons/fa';
+import { useState } from 'react';
+import { FaAlignLeft, FaUserCircle, FaCaretDown } from 'react-icons/fa';
 
 import Wrapper from '../assets/wrappers/Navbar';
+import { useAppSelector } from '../hooks/reduxHooksWrapper';
+import { RootState } from '../state/store/store';
+
+import Logo from './Logo';
+
 const Navbar = () => {
+  const [showLogout, setShowLogout] = useState(false);
+
+  const user = useAppSelector((state: RootState) => state.user.user);
+
   return (
     <Wrapper>
-      <h4>navbar</h4>
-      <FaHome />
+      <div className='nav-center'>
+        <button type='button' className='toggle-btn'>
+          <FaAlignLeft />
+        </button>
+        <div>
+          <Logo />
+          <h3 className='logo-text'>dashboard</h3>
+        </div>
+        <div className='btn-container'>
+          <button
+            type='button'
+            className='btn'
+            onClick={() => setShowLogout(!showLogout)}
+          >
+            <FaUserCircle />
+            {user?.name}
+            <FaCaretDown />
+          </button>
+          <div className={showLogout ? 'dropdown show-dropdown' : 'dropdown'}>
+            <button type='button' className='dropdown-btn'>
+              logout
+            </button>
+          </div>
+        </div>
+      </div>
     </Wrapper>
   );
 };
