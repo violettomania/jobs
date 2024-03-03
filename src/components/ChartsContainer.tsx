f37a05486cc8e57d1bb1d4698cfcbad9f192c8ad
violettomania <danielpolya@protonmail.com>
2024-03-03 17:31:31 +0100

    add fancy charts

---
+++ src/components/ChartsContainer.tsx
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+
+import Wrapper from '../assets/wrappers/ChartsContainer';
+
+import AreaChart from './AreaChart';
+import BarChart from './BarChart';
+
+interface MonthlyApplicationsProps {
+  monthlyApplications: MonthlyApplication[];
+}
+
+const ChartsContainer = ({ monthlyApplications }: MonthlyApplicationsProps) => {
+  const [barChart, setBarChart] = useState(true);
+
+  return (
+    <Wrapper>
+      <h4>Monthly Applications</h4>
+      <button type='button' onClick={() => setBarChart(!barChart)}>
+        {barChart ? 'Area Chart' : 'Bar Chart'}
+      </button>
+      {barChart ? (
+        <BarChart data={monthlyApplications} />
+      ) : (
+        <AreaChart data={monthlyApplications} />
+      )}
+    </Wrapper>
+  );
+};
+export default ChartsContainer;
--- src/components/JobStats.tsx
+++ src/components/JobStats.tsx
@@ -4,6 +4,7 @@ import { useAppDispatch, useAppSelector } from '../hooks/reduxHooksWrapper';
 import { fetchJobStats } from '../state/actions/fetchJobStats';
 import { RootState } from '../state/store/store';
 
+import ChartsContainer from './ChartsContainer';
 import JobStatsContainer from './JobStatsContainer';
 import LoadingSpinner from './LoadingSpinner';
 
@@ -23,11 +24,13 @@ const JobStats = () => {
       ) : (
         <>
           <JobStatsContainer jobStats={jobStats} />
-          {jobStats.monthlyApplications.length > 0 && null}
+          {jobStats.monthlyApplications.length > 0 && (
+            <ChartsContainer
+              monthlyApplications={jobStats.monthlyApplications}
+            />
+          )}
         </>
       )}
-      {/* <JobStatsContainer jobStats={jobStats} />
-      {jobStats.monthlyApplications.length > 0 && null} */}
     </>
   );
 };
